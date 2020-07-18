3c422d75e9ea08629735bab8ec6540b87e00a23b
Alex <alexei.sliusarev@gmail.com>
2020-07-18 22:24:25 +0300

    fix: deleted comments in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,8 @@ let edit = document.querySelector('.profile__edit');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__text_name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__text_job'); // Воспользуйтесь инструментом .querySelector()
-
+let nameInput = formElement.querySelector('.popup__text_name');
+let jobInput = formElement.querySelector('.popup__text_job');
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
@@ -17,30 +16,13 @@ function togglePopup() {
     }
 }
 
-// Находим форму в DOM
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-
-    // let nameInput = formElement.querySelector('popup__text_name');// Воспользуйтесь инструментом .querySelector()
-    // let jobInput = formElement.querySelector('popup__text_job');// Воспользуйтесь инструментом .querySelector()
-
-    // Получите значение полей из свойства value
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
     togglePopup();
-    // Вставьте новые значения с помощью textContent
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 close.addEventListener('click', togglePopup);
 edit.addEventListener('click', togglePopup);
